e5687fab6c7e3c2f72d07d10fbfd9fb680ba174e
Leandro Ferreira <leandrosf934@gmail.com>
2025-02-24 02:44:41 -0300

    fix: add morgan and fix port env

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import "dotenv/config";
 import express, { json } from "express";
 import router from "./router.js";
 import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 
+app.use(morgan('combined'))
 app.use(cors());
 app.use(json());
 app.use(router);
 
 app.listen(process.env.PORT, () => {
-  console.log("Server is running on http://localhost:3000");
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
